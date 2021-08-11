502eaee1500c2fdd0eca8899f6a6355141494c61
Roshani Duratkar <durroshani@gmail.com>
2021-08-11 23:16:04 +0530

    added s.close()

--- addition.java
+++ addition.java
@@ -3,6 +3,7 @@ class Sol{
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         int n=s.nextInt();
+        s.close();
         System.out.println("Learning Git"+n);
     }
 }
