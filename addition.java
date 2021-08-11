8cbf1a70263a4459921cb003eaf5e60a43430875
Roshani Duratkar <durroshani@gmail.com>
2021-08-11 23:10:16 +0530

    added some code

--- addition.java
+++ addition.java
@@ -1,6 +1,8 @@
-//import java.util.*;
+import java.util.*;
 class Sol{
     public static void main(String[] args) {
-        System.out.println("Learning Git");
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        System.out.println("Learning Git"+n);
     }
 }
